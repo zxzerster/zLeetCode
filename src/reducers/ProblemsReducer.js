fc10e5e16f4cf79f5fa18e1078fa6a5e5aec075c
zxzerster <zxzerster@gmail.com>
2018-12-10 10:49:12 +0800

    Remove loading & error state out from redux and add them into component related state

--- src/reducers/ProblemsReducer.js
+++ src/reducers/ProblemsReducer.js
@@ -1,9 +1,6 @@
 import { LEETCODE_ALL_PROBLEMS, LEETCODE_ALL_PROBLEMS_SUCCESS, LEETCODE_ALL_PROBLEMS_FAILED } from '../actions/types';
 
 const INITIAL_STATE = {
-    // UI
-    loading: false,
-    error: null,
     // Data
     allQuestions: null,
 };
@@ -11,16 +8,14 @@ const INITIAL_STATE = {
 export default (state = INITIAL_STATE, action) => {
     switch (action.type) {
         case LEETCODE_ALL_PROBLEMS:
-            return { ...INITIAL_STATE, loading: true, error: null };
+            return { ...INITIAL_STATE };
         case LEETCODE_ALL_PROBLEMS_SUCCESS:
         return {
             ...state,
             allQuestions: action.payload,
-            loading: false,
-            error: null,
         };
         case LEETCODE_ALL_PROBLEMS_FAILED:
-            return { ...INITIAL_STATE, loading: false, error: action.error };
+            return { ...INITIAL_STATE };
         default:
             return state;
     }
