4bea507cd31e4e2aa167a8ce7caf620e91dc44ca
zxzerster <zxzerster@gmail.com>
2018-12-29 13:12:00 +0800

    Add a native module - only a template for now

---
+++ Libraries/BackgroundWorkers/ios/RNBackgroundWorkers.m
@@ -0,0 +1,13 @@
+
+#import "RNBackgroundWorkers.h"
+
+@implementation RNBackgroundWorkers
+
+- (dispatch_queue_t)methodQueue
+{
+    return dispatch_get_main_queue();
+}
+RCT_EXPORT_MODULE()
+
+@end
+  
